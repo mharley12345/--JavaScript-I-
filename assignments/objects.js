968ab60867320ea581d147250c43e4c610d443cc
mharley12345 <47269554+mharley12345@users.noreply.github.com>
2019-05-07 04:51:14 -0700

    Update objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -11,11 +11,35 @@
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
 
-const intern1 = {id:1, name:'Mitzi',email:'mmelloy0@psu.edu',gender:'F'}
-const intern2 = {id:2,name:'Kennan',email:'kdiben1@tinypic.com',gender:'M'}
-const intern3 = {id:3, name:'Keven',email:'kmummery2@wikimedia.org',gender:'M'}
-const intern4 ={ id:4,name:'Gannie',email:'gmartinson3@illinois.edu',}
-const intern5 ={id:5,name:'Antonietta',email:'adaine5@samsung.com',gender:'F'}
+const intern1 = {
+ id:1, 
+ name:'Mitzi',
+email:'mmelloy0@psu.edu',
+gender:'F'}
+
+const intern2 = {
+id:2,
+name:'Kennan',
+email:'kdiben1@tinypic.com',
+gender:'M'}
+
+const intern3 = {
+id:3, 
+name:'Keven',
+email:'kmummery2@wikimedia.org',
+gender:'M'}
+
+const intern4 ={ 
+id:4,
+name:'Gannie',
+email:'gmartinson3@illinois.edu',
+gender'F'},
+
+const intern5 ={
+id:5,
+name:'Antonietta',
+email:'adaine5@samsung.com',
+gender:'F'}
 
 
 
